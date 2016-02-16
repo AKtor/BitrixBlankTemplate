bbaff6b8134401b3f2338d261313a23af389a3cb
Alexey Kudrow <alexey.kudrow@gmail.com>
2016-02-16 16:17:20 +0700

    Conformity to the Bitrix style

--- .default/components/bitrix/menu/one_level_menu/template.php
+++ .default/components/bitrix/menu/one_level_menu/template.php
@@ -1,12 +1,13 @@
-<?if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();
+<?if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();?>
 
+<?
 /*
 Шаблон одноуровневого меню. Ничего лишнего, только html код.
 
 Генерирует код следующего вида:
 <ul>
 	<li><a href=""></a></li>
-	<li class="active"><a href=""></a></li>
+	<li class="selected"><a href=""></a></li>
 	<li><a href=""></a></li>
 	<li><a href=""></a></li>
 	<li><a href="" target="_blank" rel="nofollow"></a></li>
@@ -17,21 +18,24 @@
 2. Добавьте необходимым ссылкам параметр ATTRIBUTES
 3. Укажите нужное значение. Например: target="_blank" rel="nofollow"
 */
+?>
 
-if (!empty($arResult)):?>
+<?if (!empty($arResult)):?>
 <ul>
-	<?
-	foreach ($arResult as $arItemKey => $arItem):
-		if (
-			$arItem['DEPTH_LEVEL'] > 1
-			|| $arItem['PERMISSION'] == 'D'
-			|| !is_int($arItemKey)
-		)
-		{
-			continue;
-		}
-	?>
-		<li<?=( $arItem['SELECTED'] ? ' class="active"' : '' )?>><a href="<?=$arItem['LINK']?>"<?=( $arItem['PARAMS']['ATTRIBUTES'] ? ' ' . $arItem['PARAMS']['ATTRIBUTES'] : '' )?>><?=$arItem['TEXT']?></a></li>
-	<?endforeach?>
+
+<?foreach ($arResult as $arItemKey => $arItem):
+	if (
+		$arItem['DEPTH_LEVEL'] > 1
+		|| $arItem['PERMISSION'] == 'D'
+		|| !is_int($arItemKey)
+	) {
+		continue;
+	}
+?>
+
+	<li<?if ($arItem['SELECTED']):?> class="selected"<?endif?>><a href="<?=$arItem['LINK']?>"<?if ($arItem['PARAMS']['ATTRIBUTES']):?> <?=$arItem['PARAMS']['ATTRIBUTES']?><?endif?>><?=$arItem['TEXT']?></a></li>
+
+<?endforeach?>
+
 </ul>
 <?endif?>
